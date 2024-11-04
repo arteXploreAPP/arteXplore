de1cc92ca0c0b45fa5ecb032f40a35c23fa106b3
arteXploreAPP <felipe.carreno@liceob72.cl>
2024-11-04 08:58:19 -0300

    Add files via upload

---
+++ Pagina.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" >
+    <link rel="stylesheet" href="style.css">
+    <title>arteXplore</title>
+
+
+</head>
+
+<body>
+<header>
+    <div><img src="Imagenes/logo nuevo.png" alt="" id="logo">
+    </div>
+
+    <div>
+    <h1>arteXplore</h1>
+    <p>Encuentra los museos mas ceranos y llamativos</p>  
+    </div>
+    <div>
+
+
+        
+    </div>
+
+
+</header>
+
+<nav>
+    <a class="link" href="registrarvista.html">Resgitrarse</a>
+    <a class="link" href="loginvista.html">Inciar sesión</a>
+    <a download=""    href="#descargar">Descargar APP</a>
+</nav>
+<div class="container">
+    <section id="inicio">
+        <h2>Bienvenido</h2>
+        <p>Descubre los museos mas hermosos y entretenidos que te ayudaran a conocer sobre la historia de Chile, o sobre otras cosas.</p>
+    </section>
+    <section id="exposiciones">
+        <h2>Museos mas famosos de Chile</h2>
+        <div class="gallery">
+            <figure>
+                <img src="Imagenes/museodelamemoria.png" alt="Museo de la Memoria y los Derechos Humanos">
+                <figcaption>Museo de la Memoria y los Derechos Humanos</figcaption>
+            </figure>
+            <figure>
+                <img src="Imagenes/museodeartepreco.png" alt="Museo Chileno de Arte Precolombino">
+                <figcaption>Museo Chileno de Arte Precolombino</figcaption>
+            </figure>
+            <figure>
+                <img src="Imagenes/museointeractivomirador.png" alt="Museo Interactivo Mirador">
+                <figcaption>Museo Interactivo Mirador</figcaption>
+            </figure>
+        </div>
+    </section>
+
+</div>
+
+<footer>
+    &copy; 2024 Liceo Comercial b72. Todos los derechos reservados.
+
+
+
+    
+</body>
+
+
+</html>
